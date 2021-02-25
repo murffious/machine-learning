28bada28f65a7d09a6ce9fd3e7672ff189da9b3b
Paul Murff <25558342+murffious@users.noreply.github.com>
2021-02-24 21:40:21 -0700

    adding ideas here

--- personal/masters_predictions.ml
+++ personal/masters_predictions.ml
@@ -5,22 +5,27 @@ How is Amazon like the Roman Empire of old if so...?
 
 
 
-Tech and Govt' Policy predictions 
+I. Tech and Govt' Policy predictions 
+Wild West and Trains- Telegraph Banks Robbers, IDEAS and Enginenering and Weapons books
+Laws 
+Tech to pass them 
 
-
-Patterns 
-
-
+II. Patterns of Rise and Fall (amazon is not falling lol) just seeking a possible correlation
 Rome
 Precursor:grew from a small town
 The man: 
 The Bubble: 
 Results Legacy: world today stands on shoulders of Ancient Rome
 
-
-
 Amazon
 Precursor: The 1993  browsers released for access to the World Wide Web and rise in personal home computer ownership, popularizing use of the Internet.
 The man: Bezos left the job he had on Wall Street and went to start Amazon in 1994 feeling it was late to the party of the dotcom biz boom. Maybe late is good.
 The Bubble: Between 1995 and 2000, peaking in 2000 then the bubble popped. 
 Results Legacy : AWS - developers/businesses stand on the shoulders of all that is great for seemingly years to come 
+
+
+III. Elon Musk and Bill Gates - Knoweldge Correlation 
+Reads a ton 
+
+
+Says he uses a learning technique to study other fields and correlate to his profession for example. 
