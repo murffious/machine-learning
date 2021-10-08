06a5464d0f75373b9c2481db7db918258db7fc1f
Paul Murff <25558342+murffious@users.noreply.github.com>
2021-10-07 20:01:16 -0600

    Create algebraForMl.py

---
+++ algebraForMl.py
@@ -0,0 +1,7 @@
+Exercises from  Basics of Linear Algebra for Machine Learning
+Discover the Mathematical Language of Data in Python
+Part 1: Foundation. Discover a gentle introduction to the field of linear algebra and the relationship it has with the field of machine learning.
+􏰀 Part 2: NumPy. Discover NumPy tutorials that show you how to create, index, slice, and reshape NumPy arrays, the main data structure used in machine learning and the basis for linear algebra examples in this book.
+􏰀 Part 3: Matrices. Discover the key structures for holding and manipulating data in linear algebra in vectors, matrices, and tensors.
+􏰀 Part 4: Factorization. Discover a suite of methods for decomposing a matrix into its constituent elements in order to make numerical operations more efficient and more stable.
+􏰀 Part 5: Statistics. Discover statistics through the lens of linear algebra and its applica- tion to principal component analysis and linear regression.
